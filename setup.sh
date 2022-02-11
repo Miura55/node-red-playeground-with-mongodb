b3e51c9c11ca7e665927267019161f90aef899a5
Miura55 <acordion.piano@gmail.com>
2022-02-11 17:35:28 +0900

    first commit

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Set the password for Node-RED
+npm install bcryptjs
+YOUR_NODERED_PASSWORD=$(openssl rand -base64 12 | fold -w 10 | head -1)
+UI_NODERED_PASSWORD_CRYPT=$(node -e "console.log(require('bcryptjs').hashSync(process.argv[1], 8));" ${YOUR_NODERED_PASSWORD})
+
+# Write password to .env file
+if [ -e ".env" ]; then
+    rm .env
+fi
+
+echo "NODE_RED_PASSWORD='${UI_NODERED_PASSWORD_CRYPT}'" >>.env
+
+docker-compose up -d
+docker-compose run --rm mongodb /tmp/createNodeREDUser.sh
+
+echo "Your Node-RED password:${YOUR_NODERED_PASSWORD}"
